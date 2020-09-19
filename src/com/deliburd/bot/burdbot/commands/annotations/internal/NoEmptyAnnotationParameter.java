475e69379a17887452d7431923f7967db458c87d
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2020-09-18 23:10:01 -0700

    Created annotations to be restrict annotation parameters

---
+++ src/com/deliburd/bot/burdbot/commands/annotations/internal/NoEmptyAnnotationParameter.java
@@ -0,0 +1,14 @@
+package com.deliburd.bot.burdbot.commands.annotations.internal;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(SOURCE)
+@Target(METHOD)
+public @interface NoEmptyAnnotationParameter {
+}
