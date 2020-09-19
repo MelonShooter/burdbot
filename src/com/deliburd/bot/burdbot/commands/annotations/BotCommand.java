e33b084c93051b57bd396fe06fe82f457fa981cc
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2020-09-18 23:23:57 -0700

    Added annotations to constrain BotCommand's annotation parameters

--- src/com/deliburd/bot/burdbot/commands/annotations/BotCommand.java
+++ src/com/deliburd/bot/burdbot/commands/annotations/BotCommand.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.deliburd.bot.burdbot.commands.annotations;
 
 import java.lang.annotation.Documented;
@@ -8,6 +5,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import com.deliburd.bot.burdbot.commands.annotations.internal.NoEmptyAnnotationParameter;
+import com.deliburd.bot.burdbot.commands.annotations.internal.NoNegativeAnnotationParameter;
+import com.deliburd.bot.burdbot.commands.annotations.internal.NoPureWhitespaceAnnotationParameter;
+import com.deliburd.bot.burdbot.commands.annotations.internal.NoWhitespaceAnnotationParameter;
+import com.deliburd.bot.burdbot.commands.annotations.internal.UniqueAnnotationParameter;
+import com.deliburd.bot.burdbot.commands.annotations.internal.WarnDuplicateElementAnnotationParameter;
 
 import net.dv8tion.jda.api.Permission;
 
@@ -19,31 +23,38 @@ import net.dv8tion.jda.api.Permission;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-
 public @interface BotCommand {
 	/**
-	 * The command's name. The default is "", which means the command name
-	 * becomes the name of the method lowercased.
+	 * The command's name (case-insensitive). The default is "", which means the command name
+	 * becomes the name of the method lowercased. This command name cannot contain any whitespace.
 	 */
+	@NoWhitespaceAnnotationParameter
+	@UniqueAnnotationParameter
 	public String commandName() default "";
 	
 	/**
-	 * The command's description
+	 * The command's description. The command description cannot be empty or contain purely whitespace.
 	 */
+	@NoEmptyAnnotationParameter
+	@NoPureWhitespaceAnnotationParameter
 	public String commandDescription();
 	
 	/**
-	 * The cooldown for the command.
+	 * The cooldown for the command. This cannot be negative.
 	 */
+	@NoNegativeAnnotationParameter
 	public long cooldown() default 5;
 	
 	/**
-	 * The command's argument descriptions
+	 * The command's argument description. This cannot contain any empty descriptions or descriptions that contain purely whitespace.
 	 */
+	@NoEmptyAnnotationParameter
+	@NoPureWhitespaceAnnotationParameter
 	public String[] argumentDescriptions();
 	
 	/**
-	 * The required permissions to execute this command
+	 * The required permissions to execute this command.
 	 */
-	public Permission[] requiredPermissions();
+	@WarnDuplicateElementAnnotationParameter
+	public Permission[] requiredPermissions() default {};
 }
